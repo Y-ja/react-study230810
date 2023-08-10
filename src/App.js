0c3611aa3de5165290e7723abd79355bcd2ae1c1
20230720 <ghkdwo34@naver.com>
2023-08-10 14:12:39 +0900

    1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const $h2 = <h2>반가워요^^</h2>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <h1>Kitsch</h1>
+      {$h2}
+      </div>
+      <div className='noname'>
+          <p>
+           우리만의 자유로운 nineteen's kitsch<br />
+           지금까지 한적 없는 custom fit<br />
+           올려 대는 나의 feed엔 like it<br />
+           홀린 듯이 눌러 모두 다 like it<br />
+           내가 추는 춤을 다들 따라 춰<br />
+           매일 너의 알고리즘에 난 떠<br />
+           걷잡을 수 없이 올라 미친 score<br />
+           그 누구도 예상 못할 nineteen's kitsch<br />
+          </p>
+      </div>
+    </>
+   
   );
 }
-
 export default App;
